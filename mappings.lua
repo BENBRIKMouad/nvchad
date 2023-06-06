761f726f4349a9b8af0238eb837e8eb9e5f99875
BENBRIKMOUD <benbikmoaud@outlook.com>
2023-06-06 22:51:28 +0100

    init nvchad custom config for golang

---
+++ mappings.lua
@@ -0,0 +1,68 @@
+local M = {}
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line"
+    },
+    ["<leader>dd"] = {
+      "<cmd>lua require('dap').continue({args= {'1'}})<CR>1<CR><CR>",
+      "debug file"
+    },
+    ["<leader>dus"] = {
+      function()
+        local widgets = require('dap.ui.widgets');
+        local sidebar = widgets.sidebar(widgets.scopes);
+        sidebar.open();
+      end,
+      "Open debugging sidebar"
+    }
+  }
+}
+
+M.dap_go = {
+  plugin = true,
+  n = {
+    ["<leader>dgt"] = {
+      function()
+        require('dap-go').debug_test()
+      end,
+      "Debug go test"
+    },
+    ["<leader>dgl"] = {
+      function()
+        require('dap-go').debug_last()
+      end,
+      "Debug last go test"
+    },
+    ["<leader>dgd"] = {
+      function()
+        local dap = require('dap')
+        dap.set_config({
+          continue = {
+            default = '1'
+          }
+        })
+        dap.continue()
+      end,
+      "Debug file"
+    },
+  }
+}
+
+M.gopher = {
+  plugin = true,
+  n = {
+    ["<leader>gsj"] = {
+      "<cmd> GoTagAdd json <CR>",
+      "Add json struct tags"
+    },
+    ["<leader>gsy"] = {
+      "<cmd> GoTagAdd yaml <CR>",
+      "Add yaml struct tags"
+    }
+  }
+}
+return M
